78c64a745171f9434756ced41f537d3c18cbb3c2
sevval gurel <146387289+purpliciouss@users.noreply.github.com>
2026-07-03 13:18:35 +0300

    Add files via upload

---
+++ trial_blackstorm/trial_blackstorm.ino
@@ -0,0 +1,17 @@
+int ledler[] = {2, 3, 4, 5 ,6 ,7};
+
+void setup() {
+  for(int i= 0; i<6; i++){
+    digitalWrite(ledler[i], HIGH);
+    delay(80);
+    digitalWrite(ledler[i], LOW);
+  }
+}
+
+void loop() {
+  for(int j= 5; j>-1; j--){
+    digitalWrite(ledler[j], HIGH);
+    delay(80);
+    digitalWrite(ledler[j], LOW);
+  }
+}
